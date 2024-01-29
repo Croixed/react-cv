be9089d946a0d33265263621f17c284bd297b8fd
at <98226985+Croixed@users.noreply.github.com>
2024-01-28 20:49:16 -0800

    finished basic rendering for cv

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,9 +4,36 @@ export default function CV({ cvInfo }) {
 
   return (
     <div className="cv-wrapper">
-      {/* <h1>{cvInfo.personal.name}</h1> */}
-      <h1>{cvInfo.personal.name || 'No Name'}</h1>
-      <p>{`${cvInfo.personal.email || 'No Email'} ${cvInfo.personal.number || 'No Phone Number'}`}</p>
+      <div className="header">
+        <h1>{cvInfo.personal.name || 'No Name'}</h1>
+        <p>{`${cvInfo.personal.email || 'No Email'} ${cvInfo.personal.number || 'No Phone Number'}`}</p>
+      </div>
+      <div className="education">
+
+        <h1>education</h1>
+        {cvInfo.schooling.map((school) => {
+          return (
+            <div className="education-card">
+              <p>{school.schoolName}</p>
+              <p>{school.fieldOfStudy}</p>
+            </div>
+          )
+        })}
+      </div>
+
+        <h1>Work Experience</h1>
+        {cvInfo.experience.map((job) => {
+          return (
+            <div className="work-card">
+              <p>{job.companyName}</p>
+              <p>{job.position}</p>
+              <p>{job.desc}</p>
+              <p>{`${job.dateStarted} to ${job.dateEnded}`}</p>
+            </div>
+          )
+        })}
+      
+
     </div>
   )
 }
