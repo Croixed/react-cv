fb9dbe3f1afa82593820c047cfb1d6459d9193f8
at <98226985+Croixed@users.noreply.github.com>
2024-01-28 17:05:52 -0800

    added basic scaffolding for project

---
+++ src/components/CV.jsx
@@ -0,0 +1,9 @@
+
+export default function CV() {
+
+  return (
+    <div className="cv">
+      CV placeholder / coming soon
+    </div>
+  )
+}
---
+++ src/components/InputForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+// I will relocate these 3 to their own components later
+function personalInput() {
+
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+function educationalInput() {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+function experienceInput() {
+  return (
+    <div className="">
+
+    </div>
+  )
+}
+
+
+
+export default function InputForm() {
+
+  return (
+    <div className="">
+    
+    </div>
+  )
+}
